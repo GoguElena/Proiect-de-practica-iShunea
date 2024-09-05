4e954142b61d93c9daed7c9e381d194b74d01812
GoguElena <158441870+GoguElena@users.noreply.github.com>
2024-09-05 15:11:58 +0300

    Hero update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,13 +26,9 @@ const Hero = () => {
             <div className="hero__image-container">
                 <div className="hero__image">
                     <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-
-                    <div className="hero__image-overlay">
-
-                    </div>
-
-
                 </div>
+                <ddv className="hero__image-overlay">
+                </ddv>
             </div>
         </div>
     )
