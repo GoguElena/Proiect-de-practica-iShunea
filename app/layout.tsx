607dd21bf37b8a0423f6e50f1155e51ab66a6f67
Gogu Elena <gogu.elena@outlook.com>
2024-09-18 12:49:27 +0300

    Product prewiew modal components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import { Urbanist } from 'next/font/google'
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 import './globals.css'
+
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -18,6 +21,8 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={font.className}>
+        <ModalProvider/>
+        <ToastProvider/>
         <Navbar />
         {children}
         <Footer/>
