96ff5149b1a48f2088a9c036fddbf9706a633285
Gogu Elena <gogu.elena@outlook.com>
2024-09-25 18:41:09 +0300

    .

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,4 +1,6 @@
 import prismadb from "@/lib/prismadb";
+import { Order } from "@prisma/client";
+
 
 export const getTotalRevenue=async (storeId:string)=>{
     const paidOrders = await prismadb.order.findMany({
@@ -14,10 +16,11 @@ export const getTotalRevenue=async (storeId:string)=>{
             }
         }
     });
-    const totalRevenue = paidOrders.reduce((total: number, order: Order) => {        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
+    const totalRevenue = paidOrders.reduce((total: number, order: Order) => {
+        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
           return orderSum+item.product.price.toNumber();
        },0)
-        return totatl+orderTotal;
+        return total + orderTotal;
     },0)
     return totalRevenue;
 }
