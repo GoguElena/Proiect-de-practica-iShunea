b4ed4230e6f11e681ca1a93791b06430c2a8ccdf
Gogu Elena <gogu.elena@outlook.com>
2024-09-25 19:31:58 +0300

    .

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -17,14 +17,18 @@ export const getTotalRevenue=async (storeId:string)=>{
         }
     });
     // Verificăm dacă `orderItems` există pe `order` înainte de a accesa proprietățile sale
+    // Calculate the total revenue
     const totalRevenue = paidOrders.reduce((total: number, order: Order) => {
+        // Check if orderItems exists and has items
         if (order.orderItems && order.orderItems.length > 0) {
             const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
                 return orderSum + item.product.price.toNumber();
             }, 0);
             return total + orderTotal;
+        } else {
+            // Handle the case when orderItems is not present
+            return total;
         }
-        return total;
     }, 0);
 
     return totalRevenue;
