9f359c009ffbab0a822462885046cdd0bab73131
Gogu Elena <gogu.elena@outlook.com>
2024-09-25 18:04:36 +0300

    .

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -14,8 +14,7 @@ export const getTotalRevenue=async (storeId:string)=>{
             }
         }
     });
-    const totalRevenue = paidOrders.reduce((total: number, order) => {
-        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
+    const totalRevenue = paidOrders.reduce((total: number, order: Order) => {        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
           return orderSum+item.product.price.toNumber();
        },0)
         return totatl+orderTotal;
